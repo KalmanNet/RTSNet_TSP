68ad352fc591fa1d69181b0861cbc0aa65c03782
XiaoyongNI <xiaoyongn@126.com>
2021-12-07 18:07:09 +0100

    add random shuffle

--- main_KITTI.py
+++ main_KITTI.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import torch
 from datetime import datetime
 from Linear_sysmdl import SystemModel
@@ -47,12 +48,12 @@ print("Observation noise 1/r2 [dB]: ", 10 * torch.log10(1/(r**2)))
 print("Process noise 1/q2 [dB]: ", 10 * torch.log10(1/(q**2)))
 
 ### T and T_test for alternative 1
-T = round(gt_data[0].size()[-1] * 0.8)
-T_test = gt_data[0].size()[-1] - T
+# T = round(gt_data[0].size()[-1] * 0.8)
+# T_test = gt_data[0].size()[-1] - T
 
 ### T and T_test for alternative 2
-# T = gt_data[0].size()[-1]
-# T_test = T
+T = gt_data[0].size()[-1]
+T_test = T
 
 m1_0 = gt_data[0][:,0]
 
@@ -72,34 +73,38 @@ cv_input = []
 test_input = []
 
 ### Alternative 1
-for sequence in gt_data:
-    T1 = round(sequence.size()[-1] * 0.7) # split for training set
-    T2 = round(sequence.size()[-1] * 0.8) # split for cv set
-    train_target.append(sequence[:,0:T1])
-    cv_target.append(sequence[:,T1:T2])
-    test_target.append(sequence[:,T2:])
+# for sequence in gt_data:
+#     T1 = round(sequence.size()[-1] * 0.7) # split for training set
+#     T2 = round(sequence.size()[-1] * 0.8) # split for cv set
+#     train_target.append(sequence[:,0:T1])
+#     cv_target.append(sequence[:,T1:T2])
+#     test_target.append(sequence[:,T2:])
 
-    noise_free_obs = torch.matmul(H_kitti, sequence)
-    obs = noise_free_obs + torch.randn_like(noise_free_obs) * r # Observations; additive Gaussian Noise
-    train_input.append(obs[:,0:T1])
-    cv_input.append(obs[:,T1:T2])
-    test_input.append(obs[:,T2:])
+#     noise_free_obs = torch.matmul(H_kitti, sequence)
+#     obs = noise_free_obs + torch.randn_like(noise_free_obs) * r # Observations; additive Gaussian Noise
+#     train_input.append(obs[:,0:T1])
+#     cv_input.append(obs[:,T1:T2])
+#     test_input.append(obs[:,T2:])
 
 ### Alternative 2
-# NumTrain = 56
-# NumCV = 8
-# NumTest = 16
-# train_target = gt_data[0:NumTrain]
-# cv_target = gt_data[NumTrain:NumTrain+NumCV]
-# test_target = gt_data[NumTrain+NumCV:NumTrain+NumCV+NumTest]
+NumTrain = 56
+NumCV = 8
+NumTest = 16
 
-# obs = []
-# for sequence in gt_data:
-#     noise_free_obs = torch.matmul(H_kitti, sequence)
-#     obs.append(noise_free_obs + torch.randn_like(noise_free_obs) * r) # Observations; additive Gaussian Noise
-# train_input = obs[0:NumTrain]
-# cv_input = obs[NumTrain:NumTrain+NumCV]
-# test_input = obs[NumTrain+NumCV:NumTrain+NumCV+NumTest]
+# Shuffle the dataset
+random.shuffle(gt_data)
+
+train_target = gt_data[0:NumTrain]
+cv_target = gt_data[NumTrain:NumTrain+NumCV]
+test_target = gt_data[NumTrain+NumCV:NumTrain+NumCV+NumTest]
+
+obs = []
+for sequence in gt_data:
+    noise_free_obs = torch.matmul(H_kitti, sequence)
+    obs.append(noise_free_obs + torch.randn_like(noise_free_obs) * r) # Observations; additive Gaussian Noise
+train_input = obs[0:NumTrain]
+cv_input = obs[NumTrain:NumTrain+NumCV]
+test_input = obs[NumTrain+NumCV:NumTrain+NumCV+NumTest]
 
 
 ##############################
