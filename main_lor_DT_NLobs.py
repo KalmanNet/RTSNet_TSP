1160bb4f6087627058f49c38525ad695f2b0b867
XiaoyongNI <57953074+XiaoyongNI@users.noreply.github.com>
2022-06-17 20:44:46 +0200

    update

--- main_lor_DT_NLobs.py
+++ main_lor_DT_NLobs.py
@@ -83,8 +83,8 @@ dataFileName = ['data_lor_v20_rq020_T100.pt']#,'data_lor_v20_r1e-2_T100.pt','dat
 
 sys_model = SystemModel(f, q[0], h_nonlinear, r[0], T, T_test, m, n)# parameters for GT
 sys_model.InitSequence(m1x_0, m2x_0)# x0 and P0
-print("Start Data Gen")
-DataGen(sys_model, DatafolderName + dataFileName[0], T, T_test,randomInit=False)
+# print("Start Data Gen")
+# DataGen(sys_model, DatafolderName + dataFileName[0], T, T_test,randomInit=False)
 print("Data Load")
 print(dataFileName[0])
 [train_input_long,train_target_long, cv_input, cv_target, test_input, test_target] =  torch.load(DatafolderName + dataFileName[0],map_location=dev)  
@@ -107,18 +107,18 @@ print("testset size:",test_target.size())
 ##########################################
 ### Evaluate Lower Bound (KS with H=I) ###
 ##########################################
-N_T = len(test_target)
-test_input_LB = []
+# N_T = len(test_target)
+# test_input_LB = []
 
-for j in range(0, N_T): 
-   # Observations; additive Gaussian Noise  
-   test_input_LB.append(test_target[j] + torch.randn_like(test_target[j]) * r[0])
+# for j in range(0, N_T): 
+#    # Observations; additive Gaussian Noise  
+#    test_input_LB.append(test_target[j] + torch.randn_like(test_target[j]) * r[0])
 
-# Model with H=I          
-sys_model_LB = SystemModel(f, q[0], h, r[0], T, T_test, m, n)
-sys_model_LB.InitSequence(m1x_0, m2x_0)
-print("Evaluate ERTS H=I")
-S_Test(sys_model_LB, test_input_LB, test_target)
+# # Model with H=I          
+# sys_model_LB = SystemModel(f, q[0], h, r[0], T, T_test, m, n)
+# sys_model_LB.InitSequence(m1x_0, m2x_0)
+# print("Evaluate ERTS H=I")
+# S_Test(sys_model_LB, test_input_LB, test_target)
 
 
 
@@ -126,12 +126,12 @@ S_Test(sys_model_LB, test_input_LB, test_target)
 ### Evaluate Filters and Smoothers ###
 ######################################
 #Evaluate EKF full
-print("Evaluate EKF full")
-[MSE_EKF_linear_arr, MSE_EKF_linear_avg, MSE_EKF_dB_avg, EKF_KG_array, EKF_out] = EKFTest(sys_model, test_input, test_target)
+# print("Evaluate EKF full")
+# [MSE_EKF_linear_arr, MSE_EKF_linear_avg, MSE_EKF_dB_avg, EKF_KG_array, EKF_out] = EKFTest(sys_model, test_input, test_target)
 
-#Evaluate RTS full
-print("Evaluate RTS full")
-[MSE_ERTS_linear_arr, MSE_ERTS_linear_avg, MSE_ERTS_dB_avg, ERTS_out] = S_Test(sys_model, test_input, test_target)
+# #Evaluate RTS full
+# print("Evaluate RTS full")
+# [MSE_ERTS_linear_arr, MSE_ERTS_linear_avg, MSE_ERTS_dB_avg, ERTS_out] = S_Test(sys_model, test_input, test_target)
 
 
 # Save results
