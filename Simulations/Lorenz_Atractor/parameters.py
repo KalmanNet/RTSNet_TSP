473383269c4937140b8a37d59b1cf60093e6a40c
XiaoyongNI <57953074+XiaoyongNI@users.noreply.github.com>
2022-06-19 16:50:11 +0200

    update

--- Simulations/Lorenz_Atractor/parameters.py
+++ Simulations/Lorenz_Atractor/parameters.py
@@ -39,8 +39,8 @@ ratio = delta_t_gen/delta_t_test
 # Length of Time Series Sequence
 # T = math.ceil(3000 / ratio)
 # T_test = math.ceil(6e6 * ratio)
-T = 100
-T_test = 100
+T = 20
+T_test = 20
 
 H_design = torch.eye(3)
 
